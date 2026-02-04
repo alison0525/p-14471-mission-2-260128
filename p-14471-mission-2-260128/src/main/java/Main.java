30665164d0aa35d6f1b132684386dd68609e9f03
alison0525 <ak501636@gmail.com>
2026-02-05 02:37:09 +0900

    1단계: 종료 기능 구현

---
+++ p-14471-mission-2-260128/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+            if (command.equals("종료")) {
+                break;
+            }
+        }
+    }
+}
