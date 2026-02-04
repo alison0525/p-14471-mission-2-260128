7dc9c88e9e431ac82ed8d7b28174fde64f110c5d
alison0525 <ak501636@gmail.com>
2026-02-05 02:59:39 +0900

    5단계: 명언 목록 죄회 기능 구현

--- p-14471-mission-2-260128/src/main/java/Main.java
+++ p-14471-mission-2-260128/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -5,11 +6,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int lastId  = 0;
 
+        ArrayList<WiseSaying> wiseSayings = new ArrayList<>();
+
         System.out.println("== 명언 앱 ==");
 
         while (true) {
             System.out.print("명령) ");
+
             String command = scanner.nextLine();
+
             if(command.equals("등록")) {
                 System.out.print("명언 : ");
                 String content = scanner.nextLine();
@@ -17,7 +22,17 @@ public class Main {
                 String author = scanner.nextLine();
                 lastId++;
                 System.out.println(lastId + "번 명언이 등록되었습니다.");
-            }else if (command.equals("종료")) {
+                wiseSayings.add(new WiseSaying(lastId, content, author));
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+
+                for(int i = wiseSayings.size()-1; i >= 0; i--) {
+                    WiseSaying temp = wiseSayings.get(i);
+                    System.out.println(temp.id + " / " + temp.author + " / " + temp.content);
+                }
+
+            } else if (command.equals("종료")) {
                 break;
             }
         }
